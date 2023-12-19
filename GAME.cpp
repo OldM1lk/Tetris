0ac2973e50de999b97cd2cd78ba313376fb6a9f1
OldM1lk <144899273+OldM1lk@users.noreply.github.com>
2023-12-19 12:16:58 +0700

    Update GAME.cpp

--- GAME.cpp
+++ GAME.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <thread>
 #include <vector>
@@ -7,7 +6,6 @@
 
 using namespace std;
 
-// Defining the screen size
 int nScreenWidth = 80;
 int nScreenHeight = 30;
 wstring tetromino[7];
@@ -20,16 +18,16 @@ int Rotate(int px, int py, int r) {
 	int pi = 0;
 
 	switch (r % 4) {
-	case 0: // 0 degrees			
+	case 0:		
 		pi = py * 4 + px;
 		break;
-	case 1: // 90 degrees			
+	case 1:			
 		pi = 12 + py - (px * 4);
 		break;
-	case 2: // 180 degrees			
+	case 2:			
 		pi = 15 - (py * 4) - px;
 		break;
-	case 3: // 270 degrees			
+	case 3:			
 		pi = 3 - py + (px * 4);
 		break;
 	}
@@ -38,21 +36,16 @@ int Rotate(int px, int py, int r) {
 }
 
 bool DoesPieceFit(int nTetromino, int nRotation, int nPosX, int nPosY) {
-	// The field is full so zero spaces
 	for (int px = 0; px < 4; ++px)
 		for (int py = 0; py < 4; ++py) {
 			int pi = Rotate(px, py, nRotation);
 
 			int fi = (nPosY + py) * nFieldWidth + (nPosX + px);
-
-			// Check that test is in bounds. Note out of bounds does
-			// not necessarily mean a fail, as the long vertical piece
-			// can have cells that lie outside the boundary, so we'll
-			// just ignore them
+			
 			if (nPosX + px >= 0 && nPosX + px < nFieldWidth) {
 				if (nPosY + py >= 0 && nPosY + py < nFieldHeight) {
 					if (tetromino[nTetromino][pi] != L'.' && pField[fi] != 0)
-						return false; // fail on first hit
+						return false;
 				}
 			}
 		}
@@ -60,18 +53,14 @@ bool DoesPieceFit(int nTetromino, int nRotation, int nPosX, int nPosY) {
 	return true;
 }
 
-int main()
-{
-	// Now, we create a screen buffer!
-
+int main() {
 	wchar_t* screen = new wchar_t[nScreenWidth * nScreenHeight];
 	for (int SomeIndex = 0; SomeIndex < nScreenWidth * nScreenHeight; ++SomeIndex) screen[SomeIndex] = L' ';
 	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
 	SetConsoleActiveScreenBuffer(hConsole);
 	DWORD dwBytesWritten = 0;
-
-	// And then the tetrominos!
-	tetromino[0].append(L"..X...X...X...X."); // Tetronimos 4x4
+	
+	tetromino[0].append(L"..X...X...X...X.");
 	tetromino[1].append(L"..X..XX...X.....");
 	tetromino[2].append(L".....XX..XX.....");
 	tetromino[3].append(L"..X..XX..X......");
@@ -84,7 +73,6 @@ int main()
 		for (int y = 0; y < nFieldHeight; ++y)
 			pField[y * nFieldWidth + x] = (x == 0 || x == nFieldWidth - 1 || y == nFieldHeight - 1) ? 9 : 0;
 
-	// Game Logic
 	bool bKey[4];
 	int nCurrentPiece = 0;
 	int nCurrentRotation = 0;
@@ -103,20 +91,14 @@ int main()
 		this_thread::sleep_for(50ms);
 		++nSpeedCount;
 		bForceDown = (nSpeedCount == nSpeed);
-
-		// No we define the user input!
-		// controls are the arrow keys(down, left and right) and piece rotation it's with Z
+		
 		for (int k = 0; k < 4; ++k)
 			bKey[k] = (0x8000 & GetAsyncKeyState((unsigned char)("\x27\x25\x28Z"[k]))) != 0;
-
-		// And here we have the game logic!
-
-		// Handling player movement
+		
 		nCurrentX += (bKey[0] && DoesPieceFit(nCurrentPiece, nCurrentRotation, nCurrentX + 1, nCurrentY)) ? 1 : 0;
 		nCurrentX -= (bKey[1] && DoesPieceFit(nCurrentPiece, nCurrentRotation, nCurrentX - 1, nCurrentY)) ? 1 : 0;
 		nCurrentY += (bKey[2] && DoesPieceFit(nCurrentPiece, nCurrentRotation, nCurrentX, nCurrentY + 1)) ? 1 : 0;
 
-		// Rotation without spinning
 		if (bKey[3]) {
 			nCurrentRotation += (bRotateHold && DoesPieceFit(nCurrentPiece, nCurrentRotation + 1, nCurrentX, nCurrentY)) ? 1 : 0;
 			bRotateHold = false;
@@ -124,23 +106,20 @@ int main()
 			bRotateHold = true;
 		}
 
-		// Force the piece down the playfield if it's time
 		if (bForceDown) {
-			// Update difficulty every 50 pieces
 			nSpeedCount = 0;
 			++nPieceCount;
 			if (nPieceCount % 50 == 0)
 				if (nSpeed >= 10) --nSpeed;
 
 			if (DoesPieceFit(nCurrentPiece, nCurrentRotation, nCurrentX, nCurrentY + 1))
-				++nCurrentY; // It can, so do it!
+				++nCurrentY;
 			else {
 				for (int px = 0; px < 4; ++px)
 					for (int py = 0; py < 4; ++py)
 						if (tetromino[nCurrentPiece][Rotate(px, py, nCurrentRotation)] != L'.')
 							pField[(nCurrentY + py) * nFieldWidth + (nCurrentX + px)] = nCurrentPiece + 1;
 
-				// Check for lines
 				for (int py = 0; py < 4; ++py)
 					if (nCurrentY + py < nFieldHeight - 1) {
 						bool bLine = true;
@@ -148,7 +127,6 @@ int main()
 							bLine &= (pField[(nCurrentY + py) * nFieldWidth + px]) != 0;
 
 						if (bLine) {
-							// Remove Line if it's complete
 							for (int px = 1; px < nFieldWidth - 1; ++px)
 								pField[(nCurrentY + py) * nFieldWidth + px] = 8;
 							vLines.push_back(nCurrentY + py);
@@ -162,28 +140,22 @@ int main()
 				nCurrentY = 0;
 				nCurrentRotation = 0;
 				nCurrentPiece = rand() % 7;
-
-				// If the player reachs the field limits, game over!
+				
 				bGameOver = !DoesPieceFit(nCurrentPiece, nCurrentRotation, nCurrentX, nCurrentY);
 			}
 		}
 
-		// Now we draw the field!
-
 		for (int x = 0; x < nFieldWidth; ++x)
 			for (int y = 0; y < nFieldHeight; ++y)
 				screen[(y + 2) * nScreenWidth + (x + 2)] = L" ABCDEFG=#"[pField[y * nFieldWidth + x]];
-
-		// Drawing the pieces
+		
 		for (int px = 0; px < 4; ++px)
 			for (int py = 0; py < 4; ++py)
 				if (tetromino[nCurrentPiece][Rotate(px, py, nCurrentRotation)] != L'.')
 					screen[(nCurrentY + py + 2) * nScreenWidth + (nCurrentX + px + 2)] = nCurrentPiece + 65;
-
-		// Socring old school way
+		
 		swprintf_s(&screen[2 * nScreenWidth + nFieldWidth + 6], 16, L"SCORE: %8d", nScore);
-
-		// Animate Line Completion
+		
 		if (!vLines.empty()) {
 			WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
 			this_thread::sleep_for(400ms);
@@ -197,12 +169,10 @@ int main()
 
 			vLines.clear();
 		}
-
-		// Display Frame by Frame
+		
 		WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
 	}
 
-	// Now, we're done!
 	CloseHandle(hConsole);
 	cout << "Game Over!! Score:" << nScore << endl;
 	system("pause");
